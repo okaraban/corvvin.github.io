026b13a766ceb605ac01b301e6690e27e4e2b55b
Kalin Pavel <pahkhan8@gmail.com>
2019-03-27 20:25:52 +0200

    remove champ

--- Champion/js/script.js
+++
@@ -1,5 +0,0 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  document.getElementById("phone").innerHTML = '<span><a href="tel:+380977061494">(097) 706-14-94</a></span>';
- } else	{
-  document.getElementById("phone").innerHTML = '<span>(0619) 42-43-44</span> ';
-}
