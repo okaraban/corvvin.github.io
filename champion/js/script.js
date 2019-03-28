2bae55bd57788951e77696629bc9b96b1d4a5541
Kalin Pavel <pahkhan8@gmail.com>
2019-03-28 22:39:59 +0200

    champ update
    logo
    menu
    scroll

--- champion/js/script.js
+++ champion/js/script.js
@@ -1,3 +1,11 @@
+$('a').click(function() {
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(sectionTo).offset().top
+    }, 1400);
+});
+
+
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   document.getElementById("phone").innerHTML = '<span><a href="tel:+380977061494">(097) 706-14-94</a></span>';
  } else	{
